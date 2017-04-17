02b027cf3dbae9bc85beeb2bf1318d65a5b65d57
TeachMeToAim <mat10rlu@student.lu.se>
2017-04-17 11:21:42 +0200

    TidyNumbers - two solutions

---
+++ Google Code Jam 2017/Problem 2 - Tidy Numbers/TidyNumbersCS/TidyNumbersCS/CountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TidyNumbersCS
+{
+    public class CountController
+    {
+        public CountController() { }
+
+        public long Control(long input)
+        {
+            while (!integerIsTidy(divideInteger(input)))
+                input = input - 1;
+
+
+            return input;
+        }
+
+        public int[] divideInteger(long input)
+        {
+            char[] charDiv = input.ToString().ToCharArray();
+            int[] intDiv = new int[charDiv.Length];
+            for (int i = 0; i < charDiv.Length; i++)
+                intDiv[i] = (int)Char.GetNumericValue(charDiv[i]);
+            return intDiv;
+        }
+
+        public bool integerIsTidy(int[] intDiv)
+        {
+            for (int i = 1; i < intDiv.Length; i++)
+                if (intDiv[i] < intDiv[i - 1])
+                    return false;
+            return true;
+        }
+
+    }
+}
---
+++ Google Code Jam 2017/Problem 2 - Tidy Numbers/TidyNumbersCS/TidyNumbersCS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TidyNumbersCS
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            string fileName = "B-large.in";
+
+            string[] lines = File.ReadAllLines(@"C:\test\proj2\" + fileName);
+
+            long[] testCases = new long[Int32.Parse(lines[0])];
+            
+            Controller ctrl = new Controller();
+            for (int i = 0; i < testCases.Length; i++)
+                testCases[i] = ctrl.Control(Int64.Parse(lines[i + 1]));
+
+            using (StreamWriter outputFile = new StreamWriter(@"C:\test\OUTPUT" + fileName))
+            {
+                for (int i = 0; i < testCases.Length; i++) {
+                    int j = i + 1;
+                outputFile.WriteLine("Case #" + j + ": " + testCases[i].ToString());
+            }
+            }
+
+        }
+
+    }
+}
